62fa88ff275b758415de7c47220c112840feae7a
dasha3926 <146557979+dasha3926@users.noreply.github.com>
2023-10-21 15:06:22 +0000

    лаба 2

---
+++ lec_2.1.py
@@ -0,0 +1,7 @@
+n = int(input())
+if n % 2 == 0 or n == 0:
+    print('Четное число')
+else:
+    print('Нечетное число')
+    
+    
---
+++ lec_2.3.py
@@ -0,0 +1,5 @@
+n = int(input())
+if n % 4 == 0 and n != 0:
+ print('Високосный')
+else:
+ print('Не високосный')
